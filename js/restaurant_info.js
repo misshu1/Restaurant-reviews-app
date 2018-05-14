78e05807dc083ab22f678e49f9a7f5b2394faefa
misshu1 <31734905+misshu1@users.noreply.github.com>
2018-05-14 23:24:21 +0300

    ajusted media queryes

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -56,8 +56,9 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
     address.innerHTML = restaurant.address;
 
     const image = document.getElementById('restaurant-img');
-    image.className = 'restaurant-img'
+    image.className = 'restaurant-img';
     image.src = DBHelper.imageUrlForRestaurant(restaurant);
+    image.alt = restaurant.name;
 
     const cuisine = document.getElementById('restaurant-cuisine');
     cuisine.innerHTML = restaurant.cuisine_type;
